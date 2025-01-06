093592cf016d4b3383d8cb78d18d7ddc497c6d72
Gabby-Tech1 <gkaquainoo@gmail.com>
2025-01-06 21:15:13 +0000

    Feat(Home): Added the navbar and the footer and working on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       {/* You can add your routing here 👇👇 */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   )
 }
