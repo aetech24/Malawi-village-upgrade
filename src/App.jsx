8bed59c3823bbbb6e1c35c028117f9e8b4937cb7
Yakubu Abdul Aziz <yakubuabdulaziz641@gmail.com>
2025-01-07 01:14:41 +0000

    Added a billing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Contact from './pages/Contact'
-import About from'./pages/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Billing from './pages/Billing';
 
 const App = () => {
+  // Props for the Billing component
+  const shippingCost = 5.0;
+
+  const handlePlaceOrder = () => {
+    alert('Order placed successfully!');
+  };
+
   return (
     <div>
       <Navbar />
-      {/* You can add your routing here 👇👇 */}
+      {/* Add your routing here */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/About" element={<About/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/billing"
+          element={<Billing shippingCost={shippingCost} onPlaceOrder={handlePlaceOrder} />}
+        />
       </Routes>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
