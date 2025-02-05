ced4d534fc3c228197a6c039e1da71b1d212de31
Stedan1234 <aetech.hub@aetechorg.com>
2025-02-05 21:25:54 +0000

    refactor: update tracking page layout and styling

--- src/pages/Tracking.jsx
+++ src/pages/Tracking.jsx
@@ -1,21 +1,20 @@
-import React from "react";
 
 const Tracking = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <div className="bg-white p-10 rounded-2xl shadow-lg w-full max-w-lg">
-        <h1 className="text-2xl font-bold text-center mb-6">Order Tracking</h1>
-        <p className="text-gray-700 text-base text-center mb-8">
-          To track your order please enter your Order ID in the box below and
-          press the "Track" button. This was given to you on your receipt and in
-          the confirmation email you should have received.
+    <div className="flex flex-col justify-center items-center w-full h-screen  shadow-lg">
+      <h1 className="font-bold text-center mb-14 text-4xl my-10">Order Tracking</h1>
+      <div className="flex flex-col justify-center  p-10 w-full max-w-lg gap-12 ">
+        <p className="text-gray-700 text-base text-center mb-8 w-full">
+          To check the status of your order, please enter your Order ID in the
+          field below and click the &quot;Track&quot; button. You can find your
+          Order ID on your receipt or in the confirmation email sent to you.
         </p>
-        <form>
+        <form className="flex flex-col justify-center  p-10 w-full max-w-lg gap-6">
           <div className="mb-6">
             <input
               type="text"
               id="orderID"
-              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+              className="w-full px-4 py-3 border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none"
               placeholder="Order ID"
             />
           </div>
@@ -23,13 +22,13 @@ const Tracking = () => {
             <input
               type="email"
               id="billingEmail"
-              className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none"
+              className="w-full px-4 py-3 border border-gray-300  focus:ring-2 focus:ring-blue-400 focus:outline-none"
               placeholder="Billing email"
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-black text-white py-3 rounded-lg hover:bg-gray-800 transition"
+            className="w-full bg-black text-white py-3  hover:bg-gray-800 transition"
           >
             Track
           </button>
